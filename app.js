348f3ab183a732a4b1c41f3c2e339e0ddcf2d418
Djalal Kurnia <diazkautsar>
2020-02-20 21:20:33 +0700

    fetching room, and questions

--- app.js
+++ app.js
@@ -8,7 +8,45 @@ const cors = require('cors')
 app.use(cors())
 
 io.on('connection', (socket) => {
-    
+    socket.on('fetchRooms', () => {
+        Room.findAll()
+            .then(rooms => {
+                socket.emit('showRooms', rooms)
+            })
+            .catch(err => {
+                socket.emit('showError', err)
+            })
+    })
+
+    socket.on('fetchLogicTopic', () => {
+        LogicTopic.findAll()
+            .then(questions => {
+                socket.emit('showLogicTopic', questions)
+            })
+            .catch(err => {
+                socket.emit('showError', err)
+            })
+    })
+
+    socket.on('fetchGeneralTopic', () => {
+        GeneralTopic.findAll()
+            .then(questions => {
+                socket.emit('showGeneralTopic', questions)
+            })
+            .catch(err => {
+                socket.emit('showError', err)
+            })
+    })
+
+    socket.on('fetchRiddleTopic', () => {
+        RiddleTopic.findAll()
+            .then(questions => {
+                socket.emit('showRiddleTopic', questions)
+            })
+            .catch(err => {
+                socket.emit('showError', err)
+            })
+    })
 })
 
 http.listen(3000, () => {
